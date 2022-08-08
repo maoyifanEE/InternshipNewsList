5c5ed103bc9ac51a8cc081ca4b66b439cc6d5ed1
maoyifan <maoyifan_ee@163.com>
2022-08-08 14:03:07 +0800

    update register fragment

--- app/src/main/java/com/example/appproject/ui/accountManager.kt
+++ app/src/main/java/com/example/appproject/ui/accountManager.kt
@@ -1,16 +1,13 @@
 package com.example.appproject.ui
 
-import android.util.Log
-import android.widget.Toast
-
 public class AccountManager {
     var index = 1
-    var userList = listOf(
-        User("18970590188","1234"),
-        User("0","0"),
-        User("0","0"),
-        User("0","0"),
-        User("0","0"),
+    var userList = mutableListOf(
+        User("Mao","ILoveMega!1314"),
+        User("Li","Random!0879"),
+        User("Liu","Random!0001"),
+        User("Zhao","Just&123"),
+        User("Zhou","Hello$123"),
         User("0","0"),
         User("0","0"),
         User("0","0"),
@@ -61,19 +58,133 @@ public class AccountManager {
         return 0
     }
 
+    fun userIsDifferent(userName: String):Boolean{
+        var result = true
+        for(pos in 0 until userList.size){
+            if(userName == userList[pos].userName){
+                result = false
+            }
+        }
+        return result
+    }
+
     fun logIn(){
         isLogin = true
     }
     fun logOut(){
         isLogin = false
     }
-    fun register(id:String,pwd:String){
-        if(index < (userList.size-1)){
-            userList[index].userName = id
-            userList[index].pwd = pwd
-            index++
+
+    /**
+     * The function register is going to be test, it will failed if
+     * the user name or password is empty
+     * the user name is already exist
+     * the user name contains sensitive or forbidden words
+     * the user name is longer than 25 characters
+     * the password is shorter than 8 characters
+     * the password does not contain number, special symbols, uppercase and lowercase at the same time
+     * the confirmed password is not the same as password
+     * the password is too easy to guess
+     */
+    fun register(userName:String, pwd:String, pwdConfirm:String):String{
+        if(userName.isEmpty()){
+            return "Please enter the user name"
+        }else if(pwd.isEmpty()){
+            return "Please enter the password"
+        }else if(pwdConfirm.isEmpty()){
+            return "Please confirm the password"
+        }else if(!userIsDifferent(userName)){
+            return "The user name is already exist"
+        }else if(!userNameIsValid(userName)){
+            return "The user name contains sensitive word"
+        }else if(userName.length > 25){
+            return "The user name is longer than 25 characters"
+        }else if(pwd.length < 8){
+            return "The password is shorted than 8 characters"
+        }else if(!pwdIsValid(pwd)){
+            return "The password must be a combination of number,special symbol,uppercase and lowercase"
+        }else if(pwdConfirm != pwd){
+            return "Please confirm the password"
+        }else if(pwdIsTooEasy(pwd,userName)){
+            return "The password is too easy to guess"
+        }else{
+            return "Successfully register"
         }
     }
+
+    /**
+     * The definition for simple password is following:
+     * Contains username
+     * Contains more than two consecutive numbers
+     * Contains high frequency word
+     * The length is shorter than 10
+     */
+    private fun pwdIsTooEasy(pwd: String,userName: String): Boolean {
+        if(pwd.contains(userName)){
+            return true
+        }else if(pwd.length < 10){
+            return true
+        }else if(pwdContainsHighFrequencyWord(pwd)){
+            return true
+        }else if(pwdContainsConsecutiveNumber(pwd)){
+            return true
+        }
+        return false
+    }
+
+    private fun pwdContainsConsecutiveNumber(pwd: String): Boolean {
+        for(i in pwd.indices){
+            if((pwd[i] in '0'..'9') && (i <= (pwd.length-2))){
+                if((pwd[i+1] == pwd[i]+1) && (pwd[i+2] == pwd[i]+2)){
+                    return true
+                }
+                if((pwd[i+1] == pwd[i]-1) && (pwd[i+2] == pwd[i]-2)){
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun pwdContainsHighFrequencyWord(pwd: String): Boolean {
+        for(i in highFrequencyWords.indices){
+            if(pwd.contains(highFrequencyWords[i])){
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun pwdIsValid(pwd: String): Boolean {
+        if(pwd.count{it.isDigit()} < 1){
+            return false
+        }else if(pwd.count{it.isUpperCase()} < 1){
+            return false
+        }else if(pwd.count{it.isLowerCase()} < 1){
+            return false
+        }else if((pwd.length - pwd.count{it.isLetterOrDigit()}) < 1){
+            return false
+        }else{
+            return true
+        }
+    }
+
+    private fun userNameIsValid(userName: String):Boolean{
+        for(i in sensitiveWords.indices){
+            if(userName.contains(sensitiveWords[i])){
+                return false
+            }
+        }
+        return true
+    }
+
+    private val sensitiveWords = listOf<String>(
+        "fuck","bitch","jerk","pervert"
+    )
+    private val highFrequencyWords = listOf<String>(
+        "hello","Hello","Hi","hi"
+    )
+
 }
 
 val userManager = AccountManager()
