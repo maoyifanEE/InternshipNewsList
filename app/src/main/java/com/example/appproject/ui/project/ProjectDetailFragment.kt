0fd89ed9865c87b8e342d9cd0e8a97eb8cced835
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-08 16:59:18 +0800

    finished

---
+++ app/src/main/java/com/example/appproject/ui/project/ProjectDetailFragment.kt
@@ -0,0 +1,33 @@
+package com.example.appproject.ui.project
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.fragment.app.Fragment
+import com.example.appproject.R
+
+class ProjectDetailFragment(private val url: String) : Fragment() {
+    private lateinit var webView: WebView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_project_detail, container, false)
+        webView = view.findViewById<WebView>(R.id.project_web_view)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        webView.loadUrl(url)
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("zyp","ProjectDetailFragmentDestroyed")
+    }
+}
