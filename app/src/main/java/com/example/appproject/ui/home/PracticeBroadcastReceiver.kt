9423a3b0b451d6956667ee58bc6b1c8dd5a2e742
WEI <weiliu2000@outlook.com>
2022-08-08 14:45:04 +0800

    Small

---
+++ app/src/main/java/com/example/appproject/ui/home/PracticeBroadcastReceiver.kt
@@ -0,0 +1,11 @@
+package com.example.appproject.ui.home
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class PracticeBroadcastReceiver:BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        
+    }
+}
