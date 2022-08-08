95aa57b6325e7e9d2e6c7eb7ccce60b234912a73
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-08 16:59:18 +0800

    finished

--- app/src/main/java/com/example/appproject/ui/project/ProjectPagerAdapter.kt
+++ app/src/main/java/com/example/appproject/ui/project/ProjectPagerAdapter.kt
@@ -1,22 +1,27 @@
 package com.example.appproject.ui.project
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import kotlin.concurrent.thread
 
-class ProjectPagerAdapter(
-    projectFragment: Fragment,
-) : FragmentStateAdapter(projectFragment) {
+class ProjectPagerAdapter(projectFragment: Fragment) : FragmentStateAdapter(projectFragment) {
+
+//    private lateinit var fragmentList: List<ProjectFragment>
 
 
-    fun isEmpty() = projectCategoryList.isEmpty()
 
     override fun getItemCount(): Int {
-        return projectCategoryList.size
+        return 5
     }
-
 
     override fun createFragment(position: Int): Fragment {
-        return ProjectFragment(projectCategoryList[position])
+        return ProjectFragment()
     }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/appproject/ui/project/ProjectSlidePagerFragment.kt
+++ app/src/main/java/com/example/appproject/ui/project/ProjectSlidePagerFragment.kt
@@ -1,6 +1,7 @@
 package com.example.appproject.ui.project
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -8,45 +9,28 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.appproject.R
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class ProjectSlidePagerFragment : Fragment() {
 
-    private lateinit var projectPagerAdapter: ProjectPagerAdapter
     private lateinit var projectViewPager: ViewPager2
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("zyp", "onCreate")
-
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("zyp", "onCreateView")
-        return inflater.inflate(R.layout.fragment_project_slide, container, false)
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val projectTabLayout = view.findViewById<TabLayout>(R.id.project_tab_layout)
-        projectPagerAdapter = ProjectPagerAdapter(this)
+        val view = inflater.inflate(R.layout.fragment_project_slide, container, false)
         projectViewPager = view.findViewById(R.id.project_view_pager)
-        projectViewPager.adapter = projectPagerAdapter
         projectViewPager.isSaveEnabled = false
-        TabLayoutMediator(projectTabLayout,projectViewPager){
-            tab,position -> tab.text = projectTabLayoutText[position]
-        }.attach()
-
-        super.onViewCreated(view, savedInstanceState)
-
+        val projectPagerAdapter = ProjectPagerAdapter(this)
+        projectViewPager.adapter = projectPagerAdapter
+        return view
     }
-
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("zyp","ProjectSlidePagerFragmentDestroyed")
     }
 }
