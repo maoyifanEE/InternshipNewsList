261ec2bc640f7f75202cfa741cc549a6b1201371
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-09 15:51:28 +0800

    finished

---
+++ app/src/main/java/com/example/appproject/MyBroadcastReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.appproject
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.example.appproject.ui.util.showToast
+import kotlin.math.log
+
+private const val TAG = "MyBroadcastReceiver"
+
+class MyBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        StringBuilder().apply {
+            append("Action: ${intent.action}")
+            append("URI: ${intent.toUri(Intent.URI_INTENT_SCHEME)}\n")
+            toString().also { log ->
+                Log.d(TAG, log)
+                log.showToast(context)
+            }
+        }
+    }
+
+}
