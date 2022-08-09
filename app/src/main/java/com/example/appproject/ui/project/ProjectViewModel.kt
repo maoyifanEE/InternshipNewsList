ca65e6022c63700264d78d8859ac15816999d0f4
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-09 14:31:52 +0800

    finished

--- app/src/main/java/com/example/appproject/ui/project/ProjectViewModel.kt
+++ app/src/main/java/com/example/appproject/ui/project/ProjectViewModel.kt
@@ -11,30 +11,10 @@ import com.example.wanandroidapi.repository.ProjectRepository
 
 class ProjectViewModel : ViewModel() {
     private val _projectResponse = MutableLiveData<ProjectData>()
-    private val _projectCategory = MutableLiveData<ProjectCategoryData>()
+
     var projectData: LiveData<ProjectData> = _projectResponse
-    var projectCategory: LiveData<ProjectCategoryData> = _projectCategory
-    var categoryId = 294
-    var pageId = 1
 
-
-
-
-    private fun getProjectCategory() {
-        ProjectRepository.getProjectsCategory(object : NetResult<ProjectCategoryData> {
-            override fun onResult(netData: NetData<ProjectCategoryData>) {
-                if (netData.errorCode == 0) {
-                    netData.data?.let {
-                        Log.d("category", it.toString())
-                        _projectCategory.postValue(it)
-                    }
-                }
-            }
-        })
-    }
-
-
-    private fun getProjectResponse() {
+    fun getProjectResponse(categoryId:Int) {
 
         ProjectRepository.getProjectList(1, categoryId, object : NetResult<ProjectData> {
             override fun onResult(netData: NetData<ProjectData>) {
@@ -45,18 +25,5 @@ class ProjectViewModel : ViewModel() {
                 }
             }
         })
-    }
-
-    fun getCid(cid: Int) {
-        categoryId = cid
-    }
-
-    fun getPage(page: Int) {
-        pageId = page
-    }
-
-    fun onRefresh() {
-        getProjectCategory()
-        getProjectResponse()
     }
 }
