acdb66243402ff43fa5008928478c8d6bf5f4466
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-10 14:31:04 +0800

    finished

--- app/src/main/java/com/example/appproject/service/BackgroundMusicService.kt
+++ app/src/main/java/com/example/appproject/service/BackgroundMusicService.kt
@@ -5,12 +5,10 @@ import android.app.Service
 import android.content.Intent
 import android.media.AsyncPlayer
 import android.media.MediaPlayer
-import android.os.Binder
 import android.os.IBinder
 import android.os.Looper
 import android.os.Message
 import android.util.Log
-import com.example.appproject.MusicPlayerActivity
 import com.example.appproject.R
 import com.example.appproject.ui.util.showToast
 
@@ -21,23 +19,27 @@ class BackgroundMusicService : Service() {
     private lateinit var musicPlayer: MediaPlayer
 
     init {
-        Log.d(TAG, "Service is running...")
+        Log.d(TAG,"Service is running...")
     }
 
+    private inner class MusicServiceHandler(msg: Message) {
 
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
 
     override fun onCreate() {
         super.onCreate()
-        musicPlayer = MediaPlayer.create(applicationContext, R.raw.rickroll)
+        "Service Created".showToast(this)
+        musicPlayer = MediaPlayer.create(applicationContext,R.raw.rickroll)
         musicPlayer.isLooping = false
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         "Service Started".showToast(this)
-        Thread {
-            musicPlayer.start()
-            Log.d(TAG, Thread.currentThread().name)
-        }.start()
+        musicPlayer.start()
         return START_STICKY
     }
 
@@ -45,10 +47,6 @@ class BackgroundMusicService : Service() {
         "Service Stopped".showToast(this)
         musicPlayer.stop()
         super.onDestroy()
-    }
-
-    override fun onBind(p0: Intent?): IBinder? {
-        return null
     }
 
 
