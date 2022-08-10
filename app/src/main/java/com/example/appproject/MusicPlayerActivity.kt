45da4334d16ad434ccfb21fdc1a6b2b1419810b6
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-10 14:31:04 +0800

    finished

--- app/src/main/java/com/example/appproject/MusicPlayerActivity.kt
+++ app/src/main/java/com/example/appproject/MusicPlayerActivity.kt
@@ -5,37 +5,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import com.example.appproject.service.BackgroundMusicService
-import android.widget.TextView
 
 class MusicPlayerActivity : AppCompatActivity() {
-    var serviceStatus: Boolean = false
-
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_player)
-        findViewById<Button>(R.id.music_service_button).let { button ->
-            button.text = "Start"
-            button.setOnClickListener {
-                Intent(this@MusicPlayerActivity, BackgroundMusicService::class.java)
-                    .let {
-                         when (serviceStatus) {
-                            false -> {
-                                startService(it)
-                                button.text = "Stop"
-                                serviceStatus = true
-                            }
-                            true -> {
-                                stopService(it)
-                                button.text = "Start"
-                                serviceStatus = false
-                            }
-
-                        }
+        val serviceButton = findViewById<Button>(R.id.music_service_button)
+        serviceButton.text = "start"
+        serviceButton.setOnClickListener {
+            when (serviceButton.text) {
+                "start" -> {
+                    Intent(this@MusicPlayerActivity, BackgroundMusicService::class.java).also {
+                        startService(it)
+                        serviceButton.text = "stop"
                     }
+                }
+                "stop" -> {
+                    Intent(this@MusicPlayerActivity,BackgroundMusicService::class.java).also {
+                        stopService(it)
+                        serviceButton.text = "start"
+                    }
+                }
             }
         }
+
     }
+
+
 }
