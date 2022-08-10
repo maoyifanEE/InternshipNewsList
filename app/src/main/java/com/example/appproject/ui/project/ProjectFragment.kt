a0dcf3823fd080aaf8999402db071514f76051d9
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-10 14:31:04 +0800

    finished

--- app/src/main/java/com/example/appproject/ui/project/ProjectFragment.kt
+++ app/src/main/java/com/example/appproject/ui/project/ProjectFragment.kt
@@ -1,31 +1,42 @@
 package com.example.appproject.ui.project
 
 import android.annotation.SuppressLint
+import android.app.ActionBar
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.appproject.MyBroadcastReceiver
 import com.example.appproject.R
+import com.example.appproject.ui.home.Test.title
 
 @SuppressLint("NotifyDataSetChanged")
 
-class ProjectFragment(private val categoryId : Int) : Fragment() {
+class ProjectFragment(private val categoryId: Int) : Fragment() {
     private val projectViewModel = ProjectViewModel()
     private lateinit var projectAdapter: ProjectAdapter
     private lateinit var progressbar: View
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        val projectBroadcastReceiver = MyBroadcastReceiver()
+        val projectFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION).apply {
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        }
+        requireActivity().registerReceiver(projectBroadcastReceiver,projectFilter)
+        requireActivity().title = "Project"
         projectAdapter = ProjectAdapter(requireActivity()) {
             onReplaceFragment(it)
         }
@@ -35,7 +46,6 @@ class ProjectFragment(private val categoryId : Int) : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val projectSwipeRefreshLayout = view.findViewById<SwipeRefreshLayout>(R.id.project_refresh)
         val projectRecyclerView = view.findViewById<RecyclerView>(R.id.project_recycler_view)
-
         projectRecyclerView.layoutManager = LinearLayoutManager(requireActivity())
 
         progressbar = view.findViewById(R.id.progress)
@@ -63,7 +73,7 @@ class ProjectFragment(private val categoryId : Int) : Fragment() {
     }
 
     private fun onReplaceFragment(url: String) {
-        parentFragmentManager
+        requireActivity().supportFragmentManager
             .beginTransaction()
             .replace(R.id.project_fragment_container, ProjectDetailFragment(url))
             .addToBackStack(url)
@@ -72,7 +82,7 @@ class ProjectFragment(private val categoryId : Int) : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("zyp","ProjectFragmentDestroyed")
+        Log.d("zyp", "ProjectFragmentDestroyed")
     }
 
 
