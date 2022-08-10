a4b6931268d500038624d7fafa8972c8dfb699e8
maoyifan <maoyifan_ee@163.com>
2022-08-10 10:30:42 +0800

    verification done

--- app/src/main/java/com/example/appproject/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/appproject/ui/setting/SettingFragment.kt
@@ -2,13 +2,8 @@ package com.example.appproject.ui.setting
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.content.BroadcastReceiver
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.ConnectivityManager
 import android.os.Bundle
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
@@ -17,16 +12,20 @@ import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.appproject.R
+import com.example.appproject.R.color.white
 import com.example.appproject.ui.userManager
+import com.example.appproject.ui.util.showToast
 import com.example.wanandroidapi.NetData
 import com.example.wanandroidapi.NetResult
 import com.example.wanandroidapi.repository.AccountRepository
 import com.google.gson.Gson
 import android.content.Context as Context1
+
 
 class SettingFragment:Fragment() {
     var idText : String = "0"
@@ -44,12 +43,15 @@ class SettingFragment:Fragment() {
         val view:View = inflater.inflate(R.layout.setting_fragment, container, false)
 
         settingViewModel = ViewModelProvider(requireActivity()).get(SettingViewModel::class.java)
+        if(context != null){
+            val myContext = context
+        }
 
 
 
 
 
-        val verifyText = Array<TextView>(8){view.findViewById(R.id.settingFragment_tv_verify0)}
+        val verifyText = Array<TextView>(10){view.findViewById(R.id.settingFragment_tv_verify0)}
         verifyText[0] = view.findViewById(R.id.settingFragment_tv_verify0)
         verifyText[1] = view.findViewById(R.id.settingFragment_tv_verify1)
         verifyText[2] = view.findViewById(R.id.settingFragment_tv_verify2)
@@ -58,6 +60,8 @@ class SettingFragment:Fragment() {
         verifyText[5] = view.findViewById(R.id.settingFragment_tv_verify5)
         verifyText[6] = view.findViewById(R.id.settingFragment_tv_verify6)
         verifyText[7] = view.findViewById(R.id.settingFragment_tv_verify7)
+        verifyText[8] = view.findViewById(R.id.settingFragment_tv_reload)
+        verifyText[9] = view.findViewById(R.id.settingFragment_tv_userAnswer)
 
         val settingFragmentEtId : EditText = view.findViewById<EditText>(R.id.settingFragment_et_userName)
         val settingFragmentEtPwd : EditText = view.findViewById<EditText>(R.id.settingFragment_et_password)
@@ -132,6 +136,8 @@ class SettingFragment:Fragment() {
 //            }else{
 //                onlineCheck(idText,passwordText)
             showWindow(view)
+            showText(verifyText)
+            verifyCheck(verifyText)
 
 //        Intent().also{intent ->
 //            intent.action = "com.example.broadcast.SETTING_NOTIFICATION"
@@ -158,6 +164,277 @@ class SettingFragment:Fragment() {
         return view
     }
 
+    @SuppressLint("ResourceAsColor")
+    private fun showText(verifyText: Array<TextView>) {
+        val i : Int = 1
+        for(i in 0..7){
+            verifyText[i].text = ""
+            verifyText[i].setTextColor(R.color.white)
+        }
+        var tem1:Int
+        var tem2:Int
+        var tem3:Int
+        var tem4:Int
+        tem1 = (0..7).random()
+        do{
+            tem2 = (0..7).random()
+        }while (tem2 == tem1)
+        do{
+            tem3 = (0..7).random()
+        }while ((tem3 == tem2) || (tem3 == tem1))
+        do{
+            tem4 = (0..7).random()
+        }while ((tem4 == tem2) || (tem4 == tem1) || (tem4 == tem3))
+        verifyText[tem1].text = verifyWord[i].one
+        verifyText[tem2].text = verifyWord[i].two
+        verifyText[tem3].text = verifyWord[i].three
+        verifyText[tem4].text = verifyWord[i].four
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun verifyCheck(verifyText: Array<TextView>) {
+        var userAnswer = mutableListOf<String>("*","*","*","*")
+        var clickFlag = mutableListOf<Boolean>(false,false,false,false,false,false,false,false)
+        var index = 0
+        verifyText[0].setOnClickListener{
+            clickFlag[0] = !clickFlag[0]
+            if(clickFlag[0]){
+                context?.let { verifyText[0].setTextColor(it.getColor(R.color.black)) }
+                userAnswer[index++] = verifyText[0].text.toString()
+            }else{
+                index--
+                verifyText[0].setTextColor(white)
+                for(i in 0..3){
+                    if(userAnswer[i] == verifyText[0].text.toString()){
+                        if(i == 3){
+                            userAnswer[i] = "*"
+                        }else{
+                            for(j in i..2){
+                                userAnswer[j] = userAnswer[j+1]
+                            }
+                        }
+                        userAnswer[3] = "*"
+                    }
+                }
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[0])){
+                verifyText[9].text = "success"
+            }
+        }
+        verifyText[1].setOnClickListener{
+            clickFlag[1] = !clickFlag[1]
+            if(clickFlag[1]){
+                context?.let { verifyText[1].setTextColor(it.getColor(R.color.black)) }
+                userAnswer[index++] = verifyText[1].text.toString()
+            }else{
+                index--
+                verifyText[1].setTextColor(white)
+                for(i in 0..3){
+                    if(userAnswer[i] == verifyText[1].text.toString()){
+                        if(i == 3){
+                            userAnswer[i] = "*"
+                        }else{
+                            for(j in i..2){
+                                userAnswer[j] = userAnswer[j+1]
+                            }
+                        }
+                        userAnswer[3] = "*"
+                    }
+                }
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[0])){
+                verifyText[9].text = "success"
+            }
+        }
+        verifyText[2].setOnClickListener{
+            clickFlag[2] = !clickFlag[2]
+            if(clickFlag[2]){
+                context?.let { verifyText[2].setTextColor(it.getColor(R.color.black)) }
+                userAnswer[index++] = verifyText[2].text.toString()
+            }else{
+                index--
+                verifyText[2].setTextColor(white)
+                for(i in 0..3){
+                    if(userAnswer[i] == verifyText[2].text.toString()){
+                        if(i == 3){
+                            userAnswer[i] = "*"
+                        }else{
+                            for(j in i..2){
+                                userAnswer[j] = userAnswer[j+1]
+                            }
+                            userAnswer[3] = "*"
+                        }
+                    }
+                }
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[0])){
+                verifyText[9].text = "success"
+            }
+        }
+        verifyText[3].setOnClickListener{
+            clickFlag[3] = !clickFlag[3]
+            if(clickFlag[3]){
+                context?.let { verifyText[3].setTextColor(it.getColor(R.color.black)) }
+                userAnswer[index++] = verifyText[3].text.toString()
+            }else{
+                index--
+                verifyText[3].setTextColor(white)
+                for(i in 0..3){
+                    if(userAnswer[i] == verifyText[3].text.toString()){
+                        if(i == 3){
+                            userAnswer[i] = "*"
+                        }else{
+                            for(j in i..2){
+                                userAnswer[j] = userAnswer[j+1]
+                            }
+                        }
+                        userAnswer[3] = "*"
+                    }
+                }
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[0])){
+                verifyText[9].text = "success"
+            }
+        }
+        verifyText[4].setOnClickListener{
+            clickFlag[4] = !clickFlag[4]
+            if(clickFlag[4]){
+                context?.let { verifyText[4].setTextColor(it.getColor(R.color.black)) }
+                userAnswer[index++] = verifyText[4].text.toString()
+            }else{
+                index--
+                verifyText[4].setTextColor(white)
+                for(i in 0..3){
+                    if(userAnswer[i] == verifyText[4].text.toString()){
+                        if(i == 3){
+                            userAnswer[i] = "*"
+                        }else{
+                            for(j in i..2){
+                                userAnswer[j] = userAnswer[j+1]
+                            }
+                        }
+                        userAnswer[3] = "*"
+                    }
+                }
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[0])){
+                verifyText[9].text = "success"
+            }
+        }
+        verifyText[5].setOnClickListener{
+            clickFlag[5] = !clickFlag[5]
+            if(clickFlag[5]){
+                context?.let { verifyText[5].setTextColor(it.getColor(R.color.black)) }
+                userAnswer[index++] = verifyText[5].text.toString()
+            }else{
+                index--
+                verifyText[5].setTextColor(white)
+                for(i in 0..3){
+                    if(userAnswer[i] == verifyText[5].text.toString()){
+                        if(i == 3){
+                            userAnswer[i] = "*"
+                        }else{
+                            for(j in i..2){
+                                userAnswer[j] = userAnswer[j+1]
+                            }
+                        }
+                        userAnswer[3] = "*"
+                    }
+                }
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[1])){
+                verifyText[9].text = "success"
+            }
+        }
+        verifyText[6].setOnClickListener{
+            clickFlag[6] = !clickFlag[6]
+            if(clickFlag[6]){
+                context?.let { verifyText[6].setTextColor(it.getColor(R.color.black)) }
+                userAnswer[index++] = verifyText[6].text.toString()
+            }else{
+                index--
+                verifyText[6].setTextColor(white)
+                for(i in 0..3){
+                    if(userAnswer[i] == verifyText[6].text.toString()){
+                        if(i == 3){
+                            userAnswer[i] = "*"
+                        }else{
+                            for(j in i..2){
+                                userAnswer[j] = userAnswer[j+1]
+                            }
+                        }
+                        userAnswer[3] = "*"
+                    }
+                }
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[0])){
+                verifyText[9].text = "success"
+            }
+        }
+        verifyText[7].setOnClickListener{
+            clickFlag[7] = !clickFlag[7]
+            if(clickFlag[7]){
+                context?.let { verifyText[7].setTextColor(it.getColor(R.color.black)) }
+                userAnswer[index++] = verifyText[7].text.toString()
+            }else{
+                index--
+                verifyText[7].setTextColor(white)
+                for(i in 0..3){
+                    if(userAnswer[i] == verifyText[7].text.toString()){
+                        if(i == 3){
+                            userAnswer[i] = "*"
+                        }else{
+                            for(j in i..2){
+                                userAnswer[j] = userAnswer[j+1]
+                            }
+                        }
+                        userAnswer[3] = "*"
+                    }
+                }
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[0])){
+                verifyText[9].text = "success"
+            }
+        }
+        verifyText[8].setOnClickListener{
+            showText(verifyText)
+            index = 0
+            for(i in 0..7){
+                clickFlag[i] = false
+            }
+            for(i in 0..3){
+                userAnswer[i] = "*"
+            }
+            verifyText[9].text = userAnswer[0]+userAnswer[1]+userAnswer[2]+userAnswer[3]
+            if(wordCheck(userAnswer,verifyWord[0])){
+                verifyText[9].text = "success"
+            }
+        }
+    }
+
+    private fun wordCheck(userAnswer: MutableList<String>, verifyWord: verifyWord): Boolean {
+        if(userAnswer[0] != verifyWord.one){
+            return false
+        }else if(userAnswer[1] != verifyWord.two){
+            return  false
+        }else if(userAnswer[2] != verifyWord.three){
+            return false
+        }else if(userAnswer[3] != verifyWord.four){
+            return false
+        }else{
+            return true
+        }
+    }
+
+
     private fun showWindow(view: View) {
         view.findViewById<FrameLayout>(R.id.settingFragment_fl_verify).visibility = VISIBLE
         view.findViewById<Button>(R.id.settingFragment_button_logIn).visibility = INVISIBLE
@@ -165,6 +442,7 @@ class SettingFragment:Fragment() {
         view.findViewById<EditText>(R.id.settingFragment_et_password).visibility = INVISIBLE
         view.findViewById<TextView>(R.id.settingFragment_tv_pwdFind2).visibility = INVISIBLE
         view.findViewById<TextView>(R.id.settingFragment_tv_agreement2).visibility = INVISIBLE
+
 
     }
 
@@ -242,11 +520,12 @@ class SettingFragment:Fragment() {
 //    }
 
     private val verifyWord = listOf<verifyWord>(
-        verifyWord("鸡","你","太","美")
+        verifyWord("鸡","你","太","美","蔡","帅","徐","坤"),
+        verifyWord("科","先","真","帅","","","","")
     )
-
 }
 
 data class User1(var coinCount: Int)
 
-data class verifyWord(val  one:String,val two:String,val three:String,val four:String)
+data class verifyWord(val one:String,val two:String,val three:String,val four:String,
+                      val fakeOne:String,val fakeTwo:String,val fakeThree: String,val fakeFour:String)
