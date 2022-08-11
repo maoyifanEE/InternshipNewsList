1cd9a933bb7f83f4228f9f9b1c37825bec20b5eb
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-11 10:10:28 +0800

    finished

--- app/src/main/java/com/example/appproject/MusicPlayerActivity.kt
+++ app/src/main/java/com/example/appproject/MusicPlayerActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.appproject.service.BackgroundMusicService
 import android.widget.TextView
 
